9c874d5eadc01f7d989a955f958b79744dd0553d
Dayana <242029@astanait.edu.kz>
2024-12-30 12:26:54 +0500

    2

--- src/Artist.java
+++ src/Artist.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class Artist {
+    private String name;
+
+    public Artist(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String toString() {
+        return name;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Artist artist = (Artist) obj;
+        return name.equals(artist.name);
+    }
+
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
