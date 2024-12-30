42b031c0d152e1e69646fe16b439fa575587bc7a
Dayana <242029@astanait.edu.kz>
2024-12-30 12:27:14 +0500

    3

--- src/Gallery.java
+++ src/Gallery.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
-
 public class Gallery {
+    private String name;
+
+    public Gallery(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String toString() {
+        return name;
+    }
+
+
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Gallery gallery = (Gallery) obj;
+        return name.equals(gallery.name);
+    }
+
+
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
