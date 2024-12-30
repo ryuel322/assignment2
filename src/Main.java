dd929d378f8437e7ac2a783cad81e58704c2649a
Dayana <242029@astanait.edu.kz>
2024-12-30 12:27:35 +0500

    4

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,57 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Artwork[] artworks = new Artwork[] {
+                new Artwork("Atameken", new Artist("Shokan Ualihanov"), new Gallery("Kazakh National Museum")),
+                new Artwork("Kazakh Girl", new Artist("Abylkhan Kasteev"), new Gallery("Kasteev State Museum of Arts")),
+                new Artwork("Nomadic Spirit", new Artist("Tleuberdy Bekmukhanbet"), new Gallery("Astana Modern Art Gallery"))
+        };
+
+
+        System.out.println("All Artworks:");
+        for (Artwork artwork : artworks) {
+            System.out.println(artwork);
         }
+
+
+        String artistName = "Abylkhan Kasteev";
+        System.out.println("\nArtworks by " + artistName + ":");
+        for (Artwork artwork : artworks) {
+            if (artwork.getArtist().getName().equals(artistName)) {
+                System.out.println(artwork);
+            }
+        }
+
+
+        String searchTitle = "Kazakh Girl";
+        System.out.println("\nSearch Result for \"" + searchTitle + "\":");
+        Artwork foundArtwork = null;
+        for (Artwork artwork : artworks) {
+            if (artwork.getArtwork().equals(searchTitle)) {
+                foundArtwork = artwork;
+                break;
+            }
+        }
+        System.out.println(foundArtwork != null ? foundArtwork : "Not Found");
+
+
+        for (int i = 0; i < artworks.length - 1; i++) {
+            for (int j = 0; j < artworks.length - i - 1; j++) {
+                if (artworks[j].getArtwork().compareTo(artworks[j + 1].getArtwork()) > 0) {
+                    Artwork temp = artworks[j];
+                    artworks[j] = artworks[j + 1];
+                    artworks[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("\nArtworks Sorted by Title:");
+        for (Artwork artwork : artworks) {
+            System.out.println(artwork);
+        }
+
+
+        System.out.println("\nComparing Two Artworks:");
+        System.out.println(artworks[0].equals(artworks[1]) ? "Artworks are the same" : "Artworks are different");
     }
 }
